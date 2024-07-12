e480f8a28a1205b536320042f46ee74686bd6a25
gnkadimeng <201384088@student.uj.ac.za>
2024-07-12 10:28:43 +0200

    weather incorp

---
+++ weatherEDA.R
@@ -0,0 +1,22 @@
+library(readxl)
+Weather_data <- read_excel("Weather data.xlsx", 
+                           col_types = c("date", "text", "numeric", 
+                                         "numeric", "text", "text", "numeric", 
+                                         "numeric", "numeric", "numeric", 
+                                         "numeric", "numeric", "numeric", 
+                                         "numeric", "numeric", "numeric", 
+                                         "numeric", "numeric", "numeric", 
+                                         "numeric", "numeric", "numeric", 
+                                         "numeric", "numeric", "numeric", 
+                                         "numeric", "numeric", "numeric", 
+                                         "numeric", "numeric", "numeric", 
+                                         "numeric", "numeric", "numeric", 
+                                         "numeric", "numeric", "numeric", 
+                                         "numeric", "numeric", "numeric", 
+                                         "numeric", "numeric", "numeric", 
+                                         "numeric", "numeric", "numeric", 
+                                         "numeric", "numeric", "numeric", 
+                                         "numeric", "numeric", "numeric", 
+                                         "numeric", "numeric", "numeric", 
+                                         "numeric", "numeric"))
+dim(Weather_data)
